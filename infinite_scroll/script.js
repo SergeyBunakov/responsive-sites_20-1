9fd580b2b584d2f60bdc805996c9e45c944c8ef8
SergeyBunakov <bunakov.sergey@gmail.com>
2021-05-30 21:28:26 +0300

    fixed script.js

--- infinite_scroll/script.js
+++ infinite_scroll/script.js
@@ -5,8 +5,8 @@ let photosArray = [];
 
 // Unsplash API
 const count = 10;
-const apiKey = 'KEY_HERE';
-const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+const apiKey = 'PASTE_YOUR_API_KEY_HERE';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // Helper Function to Set Attributes on DOM Elements
 function setAttributes(element, attributes) {
@@ -17,37 +17,46 @@ function setAttributes(element, attributes) {
 
 // Create Elements For Links & Photos, Add to DOM
 function displayPhotos() {
-//  Run function for each object in photosArray
+  // Run function for each object in photosArray
   photosArray.forEach((photo) => {
-  //  Create <a> to Link to Unsplash
+    // Create <a> to link to Unsplash
     const item = document.createElement('a');
     setAttributes(item, {
-      href: photo.link.html,
+      href: photo.links.html,
       target: '_blank',
     });
-  //  Create <img> for photo
+    // Create <img> for photo
     const img = document.createElement('img');
     setAttributes(img, {
       src: photo.urls.regular,
       alt: photo.alt_description,
       title: photo.alt_description,
     });
-  //  Put <img> inside <a>, then put both inside imageContainer Element
+    // Put <img> inside <a>, then put both inside imageContainer Element
     item.appendChild(img);
     imageContainer.appendChild(item);
   });
 }
 
+
 // Get photos from Unsplash API
 async function getPhotos() {
   try {
-const response = await fetch(apiUrl);
-photosArray = await response.json();
-displayPhotos();
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
   } catch (error) {
-  //  Catch Error Here
+    // Catch Error Here
   }
 }
+
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+    getPhotos();
+    console.log('load more');
+  }
+});
 
 // On Load
 getPhotos();
